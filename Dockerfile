366888f12bcc3536e61b678bcc9513dc6232b8bd
Jander937 <joburiticaga@cesde.net>
2024-04-29 11:40:41 -0500

    docker-file version 4

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,4 @@
-# Imagen base para el contenedor
-FROM adoptopenjdk:17-jdk
-
-# Directorio de trabajo dentro del contenedor
-WORKDIR /app
-
-# Copiar el archivo JAR de la API al contenedor
-COPY target/challenge-0.0.1-SNAPSHOT.jar original
-
-# Puerto en el que la API escucha dentro del contenedor
-EXPOSE 8080
-
-# Comando para ejecutar la API
-CMD ["java", "-jar", "challenge.jar"]
+FROM amazoncorretto:17-alpine-jdk
+MAINTAINER challenge
+COPY target/challenge-0.0.1-SNAPSHOT.jar challenge.jar
+ENTRYPOINT ["java","-jar", "/challenge.jar"]
---
+++ target/generated-sources/annotations/com/api/challenge/mapper/INoteBookMapperImpl.java
@@ -0,0 +1,50 @@
+package com.api.challenge.mapper;
+
+import com.api.challenge.dto.NoteBookDTO;
+import com.api.challenge.entities.NoteBookEntity;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-04-29T11:22:37-0500",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 22 (Oracle Corporation)"
+)
+@Component
+public class INoteBookMapperImpl implements INoteBookMapper {
+
+    @Override
+    public NoteBookDTO noteBookToDTO(NoteBookEntity noteBook) {
+        if ( noteBook == null ) {
+            return null;
+        }
+
+        NoteBookDTO noteBookDTO = new NoteBookDTO();
+
+        if ( noteBook.getId() != null ) {
+            noteBookDTO.setId( noteBook.getId().intValue() );
+        }
+        noteBookDTO.setSurname( noteBook.getSurname() );
+        noteBookDTO.setImg( noteBook.getImg() );
+        noteBookDTO.setFirstName( noteBook.getFirstName() );
+        noteBookDTO.setEmail( noteBook.getEmail() );
+
+        return noteBookDTO;
+    }
+
+    @Override
+    public List<NoteBookDTO> toDTOList(List<NoteBookEntity> noteBookEntities) {
+        if ( noteBookEntities == null ) {
+            return null;
+        }
+
+        List<NoteBookDTO> list = new ArrayList<NoteBookDTO>( noteBookEntities.size() );
+        for ( NoteBookEntity noteBookEntity : noteBookEntities ) {
+            list.add( noteBookToDTO( noteBookEntity ) );
+        }
+
+        return list;
+    }
+}
